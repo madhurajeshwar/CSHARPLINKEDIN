4aef09f33f307f27847905846f4d228d5ee4d675
Madhu <madhuv@oregonstate.edu>
2023-01-24 17:08:57 -0800

    School Library Form

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -8,9 +8,23 @@ namespace FirstConsoleApp
 {
     class Program
     {
+        public static string title = "C # ESSENTIAL TRAINING";
+        public static int whichPart;
+        public static String testString = "Raja Rajeshwar Madhu   ";
+        public static String  number = "1,234";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Welcome ");
+            whichPart = 450;
+            var message = string.Format("Welcome to {0} Part {1}!",
+            title, whichPart);
+            var stringReplaced = number.Replace(",", "");
+            int integerNumber = int.Parse(stringReplaced);
+            Console.WriteLine(integerNumber);
+            testString.Trim();
+            Console.WriteLine(testString);
+            Console.WriteLine(testString.ToUpper());
+            Console.WriteLine("Lower Characters "+ testString.ToLower());
+            Console.WriteLine(message);
         }
     }
 }
---
+++ SchoolLibrary/School.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolLibrary
+{
+    class School
+    {
+        String name { get; set; }
+        String address { get; set; }
+        String city {get; set; }
+        String state { get; set; }
+
+        String zip { get; set; }
+
+        string _twitterUserName;
+        string twitterUserName
+        {
+            get { return _twitterUserName; }
+            set
+            {   if(value.StartsWith("@"))
+                _twitterUserName = value;
+                else
+                {
+                    throw new Exception("Twitter address entered is invalid");
+                }
+            }
+        }
+
+
+    }
+}
